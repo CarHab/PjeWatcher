21a261d858ba974e443c3b89fd09b2a204b72255
Carlos Eduardo Habib Baptista Lima <carloshabib03@gmail.com>
2023-02-03 11:09:24 -0300

    Add project files.

---
+++ Watcher/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Watcher;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public Retriever? Retriever { get; set; }
+    public string CurrentUrl { get; set; } = "";
+    public DispatcherTimer? DispatcherTimer { get; set; } = null;
+    public MainWindow()
+    {
+        InitializeComponent();
+        Left = SystemParameters.WorkArea.Width - Width;
+        Top = SystemParameters.WorkArea.Height - Height;
+        
+    }
+
+    private void TextChangedEventHandler(object sender, TextChangedEventArgs args)
+    {
+        CurrentUrl = InputBox.Text;
+        DispatcherTimer?.Stop();
+
+        if (CurrentUrl is not "")
+        {
+            Retriever = new();
+            DispatcherTimer = new();
+            DispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            DispatcherTimer.Start();
+            DispatcherTimer.Interval = new TimeSpan(0, 1, 0);
+        }
+    }
+
+    private void DispatcherTimer_Tick(object sender, EventArgs e)
+    {
+        GetText();
+
+        // Forcing the CommandManager to raise the RequerySuggested event
+        CommandManager.InvalidateRequerySuggested();
+    }
+
+    public void GetText()
+    {
+        string responseText = Retriever.GetText(CurrentUrl);
+        string currentTime = DateTime.Now.ToShortTimeString();
+
+        string fullText = $"{currentTime} - {responseText}";
+
+        Output.Text = fullText;
+    }
+}
---
+++ Watcher/Retriever.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Watcher;
+public class Retriever
+{
+    public string GetText(string url)
+    {
+        Task<string> html = CallUrl(url);
+
+        string output = ParseHtml(html.Result);
+
+        return output;
+    }
+    public async Task<string> CallUrl(string url)
+    {
+        HttpClient client = new();
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+        client.DefaultRequestHeaders.Accept.Clear();
+        var response = client.GetStringAsync(url);
+        Thread.Sleep(1000);
+        return await response;
+    }
+
+    public string ParseHtml(string html)
+    {
+        HtmlDocument htmlDoc = new();
+        htmlDoc.LoadHtml(html);
+
+        HtmlNode htmlBody = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"j_id134:processoEvento:0:j_id498\"]");
+
+        string decodedText = WebUtility.HtmlDecode(htmlBody.InnerText);
+        return decodedText;
+    }
+}
