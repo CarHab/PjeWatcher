b417acf3b2f4c6e4de31b57558e24234bef08018
Carlos Eduardo Habib Baptista Lima <carloshabib03@gmail.com>
2023-03-30 16:27:20 -0300

    Fix Alt+Tab When Hidden
    Removes window from alt+tab when hidden to the tray.

--- Watcher/Views/MainWindow.xaml.cs
+++ Watcher/Views/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
 using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Threading;
 using Watcher.Services;
 
@@ -41,6 +43,13 @@ public partial class MainWindow : Window
         CaseNumber = caseNumber;
         Start();
     }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll")]
+    static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    private const int GWL_EX_STYLE = -20;
+    private const int WS_EX_APPWINDOW = 0x00040000, WS_EX_TOOLWINDOW = 0x00000080;
 
     private void Start()
     {
@@ -123,6 +132,9 @@ public partial class MainWindow : Window
     {
         if (_backgroundProcess)
             WindowState = WindowState.Minimized;
+
+        var helper = new WindowInteropHelper(this).Handle;
+        SetWindowLong(helper, GWL_EX_STYLE, (GetWindowLong(helper, GWL_EX_STYLE) | WS_EX_TOOLWINDOW));
     }
 
     private void Root_MouseDown(object sender, MouseButtonEventArgs e)
